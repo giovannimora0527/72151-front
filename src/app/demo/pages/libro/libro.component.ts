c0b54058a0038447ea1ff0480557c01c20004185
KazeVentum <kazeventum@gmail.com>
2025-03-31 15:51:16 -0500

    feat: :sparkles: Created Base code Component "Libro"

---
+++ src/app/demo/pages/libro/libro.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { LibroService } from './service/libro.service';
+
+@Component({
+  selector: 'app-libro',
+  imports: [],
+  templateUrl: './libro.component.html',
+  styleUrl: './libro.component.scss'
+})
+export class LibroComponent {
+
+   constructor(private libroService: LibroService) {
+     this.libroService.test();
+    }
+
+}
+
